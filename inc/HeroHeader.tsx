1a31583fd6037c85cd78f4cfc8c032222ab4a15d
zufichris <zufichris@gmail.com>
2025-04-25 18:44:47 +0400

    added llighting Component

--- inc/HeroHeader.tsx
+++ inc/HeroHeader.tsx
@@ -1,12 +1,13 @@
+import { cn } from "@/lib/utils"
 import Image from "next/image"
 
 
-const HeroHeader = ({image, video,overlay="none", title, className, mheight='min-h-[60vh]'}: {image?: string, video?:string, title: React.ReactNode, className?: string, mheight?: string,overlay?:"lg"|"md"|"sm"|"none"}) => {
+const HeroHeader = ({ image, video, overlay = "none", title, className, mheight = 'min-h-[60vh]' }: { image?: string, video?: string, title: React.ReactNode, className?: string, mheight?: string, overlay?: "lg" | "md" | "sm" | "none" }) => {
     return (
         <>
             <div className={`relative pt-28 backdrop-blur-${overlay} ${mheight} lg:min-h-[100vh]`}>
 
-                <div className={`absolute top-0 left-0 right-0 bottom-0 z-20 bg-black/50 backdrop-blur-${overlay}`}></div>
+                <LightingOverLay overlay={overlay} />
 
                 {image ? (
                     <Image src={image || ''} width={1920} height={1920} alt="Elchai" className="absolute top-0 right-0 bottom-0 left-0 object-center object-cover w-full h-full z-10" />
@@ -18,12 +19,45 @@ const HeroHeader = ({image, video,overlay="none", title, className, mheight='min
                     </>
                 )}
 
-                
-				<div className="main-container pb-20 pt-20 relative z-30">
-					<h1 className={`uppercase text-3xl lg:text-5xl ${className || ""}`}>{title}</h1>
-				</div>
-			</div>
+
+                <div className="main-container pb-20 pt-20 relative z-30">
+                    <h1 className={`uppercase text-3xl lg:text-5xl ${className || ""}`}>{title}</h1>
+                </div>
+            </div>
         </>
+    )
+}
+
+interface LightingProps {
+    overlay?: "lg" | "md" | "sm" | "none",
+    className1?: string,
+    className2?: string,
+}
+
+export function LightingOverLay({ overlay = "none", className1, className2 }: LightingProps) {
+    return (
+        <div className={`absolute top-0 left-0 right-0 bottom-0 z-20 bg-black/50 backdrop-blur-${overlay}`}>
+            <div className={cn("absolute -left-[780px] -top-[450px] w-[300px] h-[300px] sm:w-[1000px] sm:h-[1000px] opacity-70", className1)}>
+                <Image
+                    src="/images/gradient.png"
+                    alt=""
+                    fill
+                    sizes="(max-width: 1000px) 300px, 400px"
+                    className="object-cover"
+                    priority
+                />
+            </div>
+            <div className={cn("absolute -right-[780px] -bottom-[450px] w-[300px] h-[300px] sm:w-[1000px] sm:h-[1000px] opacity-70", className2)}>
+                <Image
+                    src="/images/gradient.png"
+                    alt=""
+                    fill
+                    sizes="(max-width: 1000px) 300px, 400px"
+                    className="object-cover"
+                    priority
+                />
+            </div>
+        </div>
     )
 }
 
